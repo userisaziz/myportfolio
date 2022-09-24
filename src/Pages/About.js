c674ff4d0362e62139189b133281df1dbbbd9b2b
Abdul Aziz <114148116+userisaziz@users.noreply.github.com>
2022-09-24 16:21:44 +0530

    Initial commit

---
+++ src/Pages/About.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./About.css";
+
+const About = () => {
+  return (
+    <div id="about" data-aos="zoom-in" data-aos-duration="1200">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-11 offset-sm-1">
+            <div className="aboutcontent">
+              <h2>About Me</h2>
+              <p>
+                An enthusiastic engineering fresher who is a self-starter and
+                capable to use technical skills for the betterment of the
+                organization. A talented and creative web developer with an eye
+                for detail looking for an entry-level role at a reputable
+                company. Has hands-on knowledge in both Frontend and Backend
+                skils
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
